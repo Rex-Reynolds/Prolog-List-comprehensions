c19f217966f5b26c500a39883a3d7db00a6c787a
Rex Austin Reynolds III <rreynolds3@elon.edu>
2014-07-14 17:57:20 -0400

    Prolog List Comprehensions

---
+++ warmup.pl
@@ -0,0 +1,72 @@
+%Rex Reynolds
+%Wish I had the foresight to take AI
+%4/19/2014
+
+% Works fine
+% Takes input of a positive Element, an un-empty list, and a positive
+% count.
+count(Element,[],0).
+count(Element,[Element|T],Number1) :-
+	count(Element,T,Number2),
+	Number1 is 1+Number2.
+count(Element,[Variable|T],Number1) :-
+	count(Element,T,Number2),
+	Number1 is Number2.
+
+
+
+%Works fine
+%Takes an input of a positive Element, two Lists, and a Number.
+takeout(Element,[],[],0).
+takeout(Element,[Element|Tail1],Tail2,NumOccur):-
+	!,
+	takeout(Element,Tail1,Tail2,NumOccur2),
+	NumOccur is NumOccur2 +1.
+takeout(Element,[Head|Tail1],[Head|Tail2],Variable):-
+	takeout(Element,Tail1,Tail2,Variable).
+
+
+
+
+% Does not work
+% Takes an input of two lists, a positive element, and a number.
+putIn([],Element,0,[]).
+putIn(List1,Element,NumOccur,List2):-
+	List3=[Element,List1],NumOccur2 is NumOccur -1,
+	putIn(List2,Element,NumOccur2,List3),List2=List3.
+
+
+
+
+%Does not work
+%Takes a positive element, two lists, and a number.
+deleteNum(Element,[],[],0).
+deleteNum(Element,[Head|Tail],List1,Variable):-
+	not(Head = Element),
+	deleteNum(Element,Tail,List2,Variable),
+	List1 = [Head|List2].
+deleteNum(Element,[Head|Tail],List1,Variable):-
+	-Head=Element,
+	Variable1 is Variable-1,
+	deleteNum(Element,Tail,List2,Variable1),
+	List1 = List2.
+
+
+
+
+%Does not work.
+%Takes two lists and two "sets".
+%Uses deleteNum in order to remove the group of fours from the list.
+%Uses count to group numbers of four.
+groupFours([],Set,[],Set).
+groupFours([Head|Tail],Set,NewHand,NewSet):-
+	count(Head,[Head|Tail],Number),
+	Number < 4,
+	groupFours(Tail,Set,NewHand2,NewSet),
+	NewHand = [Head|NewHand2].
+groupFours([Head|Tail],Set,NewHand,NewSet):-
+	count(Head,[Head|Tail],Number),
+	not(Number < 4),
+	deleteNum(Head,[Head|Tail],NewList,4),
+	groupFours(NewList,Set,NewHand,NewSet2),
+	NewSet = [Head|NewSet2].
